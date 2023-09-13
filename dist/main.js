9ba3c63d29a199685a2b7404453b706fb4631875
Kamila <kamilagrzadko@gmail.com>
2023-09-13 12:24:34 +0200

    arr and obj enum tuple

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,57 @@
-let myName = "Kama";
-let meaningOfLife;
-let isLoading;
-let album;
-let postId; //union type
-myName = "Andrzej";
-meaningOfLife = 45;
-isLoading = true;
-album = "elo";
-const sum = (a, b) => a + b;
-console.log(sum("t", 5));
+//Array
+let stringArr = ["one", "hey", "Kama"];
+let guitars = ["Starat", "Les Paul", 5150];
+let mixedData = ["EVH", 1984, true];
+stringArr[0] = "AAA";
+stringArr.push("42");
+guitars[0] = 1234;
+guitars.unshift("Jim");
+console.log(stringArr);
+let test = [];
+let bands = [];
+bands.push("Van Halen");
+//Tuple
+let myTuple = ["David", 42, true];
+let mixed = ["John", 1, false];
+mixed = myTuple;
+myTuple[1] = 42;
+//Object
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: "Kama",
+    prop2: true,
+};
+exampleObj.prop2 = false;
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OUT"]
+};
+let jp = {
+    name: "Jimmy",
+    active: false,
+    albums: ["313", "dddd", "qqq"]
+};
+evh = jp;
+//Funkcje
+const greetGuitairst = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}`;
+    }
+    return "hello!";
+};
+console.log(greetGuitairst(jp));
+// Enumy
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,90 @@
-let myName: string = "Kama"
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
-let postId: string | number; //union type
+//Array
 
-myName = "Andrzej";
-meaningOfLife = 45;
-isLoading = true;
-album= "elo"
+let stringArr = [ "one", "hey","Kama"];
 
-const sum = (a: string, b:number) => a + b 
+let guitars = ["Starat", "Les Paul", 5150]
 
-console.log(sum("t",5)) 
+let mixedData = ["EVH", 1984, true]
+
+stringArr[0] = "AAA"
+stringArr.push("42")
+
+guitars[0] = 1234
+
+guitars.unshift("Jim")
+
+console.log(stringArr)
+
+let test = []
+let bands: string[] = []
+
+bands.push("Van Halen")
+
+//Tuple
+
+let myTuple: [string, number, boolean] = ["David", 42, true]
+
+let mixed = ["John", 1, false]
+
+mixed = myTuple
+myTuple[1] = 42
+
+//Object
+
+let myObj: object
+
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+    prop1: "Kama",
+    prop2: true,
+}
+
+exampleObj.prop2 = false
+
+type Guitarist = {
+    name?: string,
+    active: boolean,
+    albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OUT"]
+
+}
+
+let jp: Guitarist = {
+    name: "Jimmy",
+    active: false,
+    albums: ["313", "dddd", "qqq"]
+
+}
+evh = jp
+
+//Funkcje
+
+const greetGuitairst = (guitarist : Guitarist) => {
+    if(guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}`
+    }
+    return "hello!"
+}
+
+console.log(greetGuitairst(jp))
+
+// Enumy
+
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.U)
